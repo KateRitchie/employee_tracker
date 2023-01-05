f7343ca3c530887b227b9fec86741a4acfd01f05
KateRitchie <ritchie_ecen@hotmail.com>
2023-01-04 17:55:33 -0700

    fixed js file, added console.table npm

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 // Import and require mysql2
 const mysql = require('mysql2');
 //Protect SQL login info
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Using mysql2 this logs into the mySQL database
 const db = mysql.createConnection(
@@ -18,5 +15,38 @@ const db = mysql.createConnection(
     password: process.env.DB_PASSWORD,
     database: 'employeesDB'
   },
-  console.log(`Connected to the employeesDB database.`)
 );
+
+// Connect database
+db.connect(err => {
+  if(err) throw err;
+  console.log(`Connected to the employeesDB database.`)
+  startTracker();
+});
+
+const startTracker = function() {
+  inquirer.prompt([{
+    type: 'list',
+    name: 'prompt',
+    message: 'What would you like to do?',
+    choices: [
+      'View all departments',
+      'View all roles',
+      'View all employees',
+      'Add a department',
+      'Add a role',
+      'Add an employee',
+      'Update employee role',
+      'Exit'
+    ]
+  }]).then((choice) => {
+    if (choice.prompt === 'View all departments') {
+      db.query(`SELECT * FROM department`, (err, result) => {
+        if(err) throw err;
+        console.table(result);
+        startTracker();
+      })
+    }
+  })
+
+}
